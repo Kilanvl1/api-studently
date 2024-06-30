3a2c298ba49091a6e39156db0558a979b001e9d2
Kilan_vanloo <Kilanvanloo@gmail.com>
2024-06-30 17:20:06 +0200

    added studently to cors

--- studently/settings.py
+++ studently/settings.py
@@ -155,4 +155,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ORIGIN_ALLOW_ALL = False
 
-CORS_ORIGIN_WHITELIST = ("http://localhost:3000",)
+CORS_ORIGIN_WHITELIST = ("http://localhost:3000", "https://www.student-ly.com")
