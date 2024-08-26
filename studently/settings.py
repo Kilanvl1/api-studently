e05026ca154ba311c0b83f9fa2456953f73ef460
Kilan_vanloo <Kilanvanloo@gmail.com>
2024-08-26 23:09:23 +0200

    Added acceptance to cros

--- studently/settings.py
+++ studently/settings.py
@@ -156,4 +156,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ORIGIN_ALLOW_ALL = False
 
-CORS_ORIGIN_WHITELIST = ("http://localhost:3000", "https://www.student-ly.com")
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "https://www.student-ly.com",
+    "https://yan-lime.vercel.app",
+)
